fb79f07ea38ee9cdae4f0c4d3b1583591076114e
HersheyZinc <75511044+HersheyZinc@users.noreply.github.com>
2022-06-20 19:03:47 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -23,6 +23,19 @@ def load_image(imfile):
     return img[None].to(DEVICE)
 
 
+def save_flo(flow, filename):
+    flow = flow[0].permute(1,2,0).cpu().numpy()
+    f = open(filename, 'wb')
+    magic = np.array([202021.25], dtype=np.float32)
+    height, width = flow.shape[:2]
+    magic.tofile(f)
+    np.int32(width).tofile(f)
+    np.int32(height).tofile(f)
+    data = np.float32(flow).flatten()
+    data.tofile(f)
+    f.close() 
+
+
 def viz(img, flo):
     img = img[0].permute(1,2,0).cpu().numpy()
     flo = flo[0].permute(1,2,0).cpu().numpy()
@@ -31,12 +44,13 @@ def viz(img, flo):
     flo = flow_viz.flow_to_image(flo)
     img_flo = np.concatenate([img, flo], axis=0)
 
-    # import matplotlib.pyplot as plt
-    # plt.imshow(img_flo / 255.0)
-    # plt.show()
+    #import matplotlib.pyplot as plt
+    #plt.imshow(img_flo / 255.0)
+    #plt.show()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.waitKey()
+    #cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    #cv2.waitKey()
+    cv2.imwrite("test.jpg", flo)
 
 
 def demo(args):
@@ -47,11 +61,18 @@ def demo(args):
     model.to(DEVICE)
     model.eval()
 
+    outpath = args.outpath
+    os.makedirs(outpath, exist_ok=True)
+    for_path, back_path = os.path.join(outpath, "forward_flow"), os.path.join(outpath, "backward_flow")
+    os.makedirs(for_path, exist_ok=True)
+    os.makedirs(back_path, exist_ok=True)
+
     with torch.no_grad():
         images = glob.glob(os.path.join(args.path, '*.png')) + \
                  glob.glob(os.path.join(args.path, '*.jpg'))
         
         images = sorted(images)
+        i=0
         for imfile1, imfile2 in zip(images[:-1], images[1:]):
             image1 = load_image(imfile1)
             image2 = load_image(imfile2)
@@ -60,7 +81,10 @@ def demo(args):
             image1, image2 = padder.pad(image1, image2)
 
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+            name = "{:05d}.flo".format(i)
+            save_flo(flow_low, os.path.join(back_path, name))
+            save_flo(flow_up, os.path.join(for_path, name))
+            i+=1
 
 
 if __name__ == '__main__':
@@ -70,6 +94,7 @@ if __name__ == '__main__':
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
+    parser.add_argument('--outpath',help="path to store flow output")
     args = parser.parse_args()
 
     demo(args)
